b74d07502518114c14fd37857ae1eb4ec832fec5
sami-ibrahim <sami.ibrahim@rovisys.com>
2023-07-10 13:36:07 -0400

    Solution

--- 266A - Stones on the Table.py
+++ 266A - Stones on the Table.py
@@ -1,6 +1,13 @@
 n = int(input())
 s = input()
 
+if n != len(s):
+    exit
 
-for i in s:
-    
+count = 0
+
+for i in range(len(s) - 1):
+    if s[i] == s[i + 1]:
+        count += 1
+        
+print(count)

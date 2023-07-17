d7f269b13fd5d61e90f272d16633c5957a0b46b4
sami-ibrahim <sami.ibrahim@hotmail.com>
2023-07-17 13:06:21 -0400

    solution

---
+++ 110A - Nearly Lucky Number.py
@@ -0,0 +1,6 @@
+# the lucky digits 4 and 7. For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
+
+# if "4" or "7" -> nearly lucky 
+
+# I am assuming from the first sample that if len(NearlyLuckyNumbers) > len(NonLuckyNumbers) then it is "YES" 
+
